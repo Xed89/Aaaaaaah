b2f4eed9b200055df79a2ab96164978ba740b9c3
Xed89 <simone.bosc@gmail.com>
2025-11-24 23:38:41 +0100

    docs

---
+++ docs/assets/AppBundle/main.js
@@ -0,0 +1,25 @@
+﻿import { dotnet } from './_framework/dotnet.js'
+
+const runtime = await dotnet
+    .withDiagnosticTracing(false)
+    .create();
+
+const config = runtime.getConfig();
+const exports = await runtime.getAssemblyExports(config.mainAssemblyName);
+
+const canvas = document.getElementById('canvas');
+dotnet.instance.Module['canvas'] = canvas;
+
+function mainLoop() {
+    exports.RaylibWasm.Application.UpdateFrame(canvas.clientWidth, canvas.clientHeight);
+
+    window.requestAnimationFrame(mainLoop);
+}
+
+await runtime.runMain();
+window.requestAnimationFrame(mainLoop);
+
+var peer = new Peer();
+peer.on('open', function (id) {
+    console.log('My peer ID is: ' + id);
+});
